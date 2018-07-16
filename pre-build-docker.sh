d74426e3ed08d060c8d29a96653a644e93a57bff
hari-ennekat <41279977+hari-ennekat@users.noreply.github.com>
2018-07-16 11:27:54 +0530

    pre-build-docker.sh
    This script need to be executed as a build step for every jenkins/manual docker deployment.

---
+++ pre-build-docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#This script need to be executed as a build step for every jenkins docker deployment
+#Author @HariEnnekat
+
+#can read it from buildjob itself w/o hard coding, uncomment and use below variable
+#pattern=$docker_jobname
+
+pattern="my-docker-service"
+
+function cleanup {
+	docker ps -a | grep $pattern | awk '{print $1}' | xargs docker stop
+	sleep 1s
+	docker ps -a | grep $pattern | awk '{print $1}' | xargs docker rm
+	sleep 1s
+	docker images -a | grep $pattern | awk '{print $3}' | xargs docker rmi
+	sleep 1s
+}
+container_check=`docker ps -a | grep $pattern | awk '{print $1}'`
+
+if [ ! -z "$container_check" ]; then
+cleanup
+fi
+
+unused_image_check=`docker ps -a -f status=exited -q`
+if [ ! -z "$unused_image_check" ]; then
+	docker rm $(docker ps -a -f status=exited -q)
+fi
