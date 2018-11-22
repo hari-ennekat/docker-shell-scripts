f92ef36541c0cd996562c7948462974e67638b67
hari-ennekat <41279977+hari-ennekat@users.noreply.github.com>
2018-11-22 19:03:07 +0530

    Updated the script
    Even the docker container fails, the image will be cleared. Previously this wasn't handled.

--- pre-build-docker.sh
+++ pre-build-docker.sh
@@ -5,15 +5,13 @@
 #can read it from buildjob itself w/o hard coding, uncomment and use below variable
 #pattern=$docker_jobname
 
-pattern="my-docker-service"
+pattern="$1"
 
 function cleanup {
-	docker ps -a | grep $pattern | awk '{print $1}' | xargs docker stop
-	sleep 1s
-	docker ps -a | grep $pattern | awk '{print $1}' | xargs docker rm
-	sleep 1s
-	docker images -a | grep $pattern | awk '{print $3}' | xargs docker rmi
-	sleep 1s
+        docker ps -a | grep $pattern | awk '{print $1}' | xargs docker stop
+        sleep 1s
+        docker ps -a | grep $pattern | awk '{print $1}' | xargs docker rm
+        sleep 1s
 }
 container_check=`docker ps -a | grep $pattern | awk '{print $1}'`
 
@@ -21,7 +19,13 @@ if [ ! -z "$container_check" ]; then
 cleanup
 fi
 
+image_check=`docker images -a | grep $pattern | awk '{print $3}'`
+if [ ! -z $image_check ]; then
+docker images -a | grep $pattern | awk '{print $3}' | xargs docker rmi
+sleep 1s
+fi
+
 unused_image_check=`docker ps -a -f status=exited -q`
 if [ ! -z "$unused_image_check" ]; then
-	docker rm $(docker ps -a -f status=exited -q)
+        docker rm $(docker ps -a -f status=exited -q)
 fi
